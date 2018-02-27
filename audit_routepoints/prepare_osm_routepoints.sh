8f4ebf0c97dcdadc136428d1b01a24ed7c4cbd48
Noémie <noemie.lehuby@zaclys.net>
2018-02-27 08:07:33 +0100

    màj data

--- audit_routepoints/prepare_osm_routepoints.sh
+++ audit_routepoints/prepare_osm_routepoints.sh
@@ -11,11 +11,6 @@ cat audit_lines.csv |xsv search -s osm:ref:FR:STIF:ExternalCode_Line '^$' -v > a
 cat audit_routes.csv |xsv search -s destination '^$' -v|xsv search -s code '^$' -v > audit_routes_with_destination.csv
 cat audit_stops.csv |xsv search -s osm:ref:FR:STIF '^$' -v |xsv search -s osm:public_transport platform |xsv search -s name '^$' -v > audit_stops_ok.csv
 
-#TODO : il faudrait aussi retirer
-#les doublons sur les codes STIF de lignes
-#les codes qui n'existent plus
-#etc
-
 echo "##### on fusionne le tout #####"
 xsv join stop_id osm-transit-extractor_route_stops.csv stop_point_id audit_stops_ok.csv |xsv select route_id,stop_id,name,osm:ref:FR:STIF > audit_stops_with_route_id.csv
 xsv join route_id audit_stops_with_route_id.csv route_id audit_routes_with_destination.csv |xsv select stop_id,name,code,destination,osm:ref:FR:STIF,route_id > audit_stops_with_route_info.csv

0c8fe85dc5cf04835983e6c53d4567d455a0bc78
Noémie <noemie.lehuby@zaclys.net>
2018-03-01 20:57:00 +0100

    audit routepoints - corrections des colonnes extraites

--- audit_routepoints/prepare_gtfs_routepoints.sh
+++ audit_routepoints/prepare_gtfs_routepoints.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-echo "Step 1"
-xsv join trip_id gtfs_audit_stoptime_lite.csv trip_id gtfs_trips.txt |xsv select 'route_id,stop_id,direction_id,trip_id' > gtfs_audit_trippoints.csv
+echo " Extraction des routepoints GTFS"
+echo "##### on calcule des trip points #####"
+xsv join trip_id gtfs_trips.txt trip_id gtfs_audit_stoptime_lite.csv  |xsv select 'route_id,stop_id,direction_id,trip_id' > gtfs_audit_trippoints.csv
 
-echo "Step 2"
+echo "##### on ajoute les destinations et on filtre les duplicats #####"
 xsv join trip_id gtfs_audit_trippoints.csv trip_id gtfs_audit_trip_destinations.csv |xsv select 'route_id,stop_id,direction_id,trip_dest_id' |xsv sort |uniq > gtfs_audit_routepoints_1.csv
 
-echo "Step 3"
+echo "##### on ajoute les infos de l'arrêt #####"
 xsv join stop_id gtfs_audit_routepoints_1.csv stop_id gtfs_stop_extensions.txt |xsv select '!stop_id[1]' > gtfs_audit_routepoints_2.csv
-
-echo "Step 4"
 xsv join stop_id gtfs_audit_routepoints_2.csv stop_id gtfs_stops.txt |xsv select 'stop_id,route_id,ZDEr_ID_REF_A,stop_name,stop_lat,stop_lon,wheelchair_boarding,trip_dest_id' > gtfs_audit_routepoints_3.csv
 
-echo "Step 5"
+echo "##### on ajoute le nom de l'arrêt de destination #####"
 xsv join trip_dest_id gtfs_audit_routepoints_3.csv stop_id gtfs_stops.txt |xsv select 'stop_id[0],route_id,ZDEr_ID_REF_A,stop_name[0],stop_lat[0],stop_lon[0],wheelchair_boarding,stop_name[1]' > gtfs_audit_routepoints_4.csv
 
-echo "Step 6"
-echo "stop_id,route_id,ZDEr_ID_REF_A,stop_name,stop_lat,stop_lon,wheelchair_boarding,dest_name" > gtfs_routepoints.csv
-tail -n +2 gtfs_audit_routepoints_4.csv >> gtfs_routepoints.csv 
+echo "##### on renomme pour éviter les doublons de colonnes #####"
+echo "stop_id,route_id,ZDEr_ID_REF_A,stop_name,stop_lat,stop_lon,wheelchair_boarding,dest_name" > gtfs_audit_gtfs_routepoints.csv
+tail -n +2 gtfs_audit_routepoints_4.csv >> gtfs_audit_gtfs_routepoints.csv
+
+echo "##### on affiche les informations sur la ligne #####"
+xsv join route_id gtfs_audit_gtfs_routepoints.csv route_id gtfs_routes.txt | xsv select stop_id,route_id,ZDEr_ID_REF_A,stop_name,stop_lat,stop_lon,wheelchair_boarding,dest_name,route_short_name,agency_id > gtfs_audit_routepoints_with_code.csv
+xsv join agency_id gtfs_audit_routepoints_with_code.csv agency_id gtfs_agency.txt | xsv select stop_id,route_id,ZDEr_ID_REF_A,stop_name,stop_lat,stop_lon,wheelchair_boarding,dest_name,route_short_name,agency_name > gtfs_routepoints.csv
+
+echo "##### on nettoie, on n'est pas des porcs ;) #####"
+rm gtfs_audit_*

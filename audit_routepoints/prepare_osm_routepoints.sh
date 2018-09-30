47031faf160487bc4a544e08f0ed6dfee9e86633
Noémie <noemie.lehuby@zaclys.net>
2018-09-30 13:03:47 +0200

    audit - extraction de l'accessibilité des routepoints

--- audit_routepoints/prepare_osm_routepoints.sh
+++ audit_routepoints/prepare_osm_routepoints.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-echo " Extraction des routepoints OSM"
+echo "Extraction des routepoints OSM"
 echo "##### on retire les colonnes qu'on n'utilisera pas #####"
-cat osm-transit-extractor_lines.csv |xsv select line_id,network,osm:ref:FR:STIF:ExternalCode_Line > audit_lines.csv
-cat osm-transit-extractor_routes.csv |xsv select route_id,code,destination > audit_routes.csv
+cat osm-transit-extractor_lines.csv |xsv select line_id,network,osm:ref:FR:STIF:ExternalCode_Line,osm:wheelchair > audit_lines.csv
+cat osm-transit-extractor_routes.csv |xsv select route_id,code,destination,osm:wheelchair > audit_routes.csv
 
-#echo "##### on retire les lignes des fichiers avec des infos manquantes #####"
-#cat audit_lines.csv |xsv search -s osm:ref:FR:STIF:ExternalCode_Line '^$' -v > audit_lines_with_mapping.csv
-#cat audit_routes.csv |xsv search -s destination '^$' -v|xsv search -s code '^$' -v > audit_routes_with_destination.csv
-#cat osm-transit-extractor_stop_points.csv |xsv search -s osm:ref:FR:STIF '^$' -v |xsv search -s osm:public_transport platform |xsv search -s name '^$' -v > audit_stops_ok.csv
-
-echo "##### on fusionne le tout #####"
+echo "##### on collecte les infos des arrêts et des parcours #####"
 xsv join stop_id osm-transit-extractor_route_stops.csv stop_point_id osm-transit-extractor_stop_points.csv |xsv select route_id,stop_id,name,osm:ref:FR:STIF,osm:ref:FR:STIF,osm:ref:FR:STIF:stop_id,osm:wheelchair,lat,lon > audit_stops_with_route_id.csv
-xsv join route_id audit_stops_with_route_id.csv route_id audit_routes.csv |xsv select stop_id,name,code,destination,osm:ref:FR:STIF,osm:ref:FR:STIF:stop_id,osm:wheelchair,route_id,lat,lon > audit_stops_with_route_info.csv
-xsv join route_id audit_stops_with_route_info.csv route_id osm-transit-extractor_line_routes.csv|xsv select stop_id,name,code,destination,osm:ref:FR:STIF,osm:ref:FR:STIF:stop_id,osm:wheelchair,line_id,route_id,lat,lon > audit_stops_with_line_id.csv
-xsv join line_id audit_stops_with_line_id.csv line_id audit_lines.csv |xsv select stop_id,name,code,destination,network,osm:ref:FR:STIF,osm:ref:FR:STIF:ExternalCode_Line,osm:wheelchair,line_id,route_id,lat,lon,osm:ref:FR:STIF:stop_id > osm_routepoints.csv
+xsv join route_id audit_stops_with_route_id.csv route_id audit_routes.csv |xsv select stop_id,name,code,destination,osm:ref:FR:STIF,osm:ref:FR:STIF:stop_id,osm:wheelchair[0],osm:wheelchair[1],route_id,lat,lon > audit_stops_with_route_info.csv
+echo "stop_id,name,code,destination,osm:ref:FR:STIF,osm:ref:FR:STIF:stop_id,stop_wheelchair,route_wheelchair,route_id,lat,lon" > audit_stops_with_route_info_ok.csv
+tail -n +2 audit_stops_with_route_info.csv >> audit_stops_with_route_info_ok.csv
+
+echo "##### on ajoute les infos des lignes #####"
+xsv join route_id audit_stops_with_route_info_ok.csv route_id osm-transit-extractor_line_routes.csv|xsv select stop_id,name,code,destination,osm:ref:FR:STIF,osm:ref:FR:STIF:stop_id,stop_wheelchair,route_wheelchair,line_id,route_id,lat,lon > audit_stops_with_line_id.csv
+xsv join line_id audit_stops_with_line_id.csv line_id audit_lines.csv |xsv select stop_id,name,code,destination,network,osm:ref:FR:STIF,osm:ref:FR:STIF:ExternalCode_Line,stop_wheelchair,route_wheelchair,osm:wheelchair,line_id,route_id,lat,lon,osm:ref:FR:STIF:stop_id > audit_stops_with_line_info.csv
+echo "stop_id,name,code,destination,network,osm:ref:FR:STIF,osm:ref:FR:STIF:ExternalCode_Line,stop_wheelchair,route_wheelchair,line_wheelchair,line_id,route_id,lat,lon,osm:ref:FR:STIF:stop_id" > osm_routepoints.csv
+tail -n +2 audit_stops_with_line_info.csv >> osm_routepoints.csv
 
 echo "##### on nettoie les fichiers temporaires #####"
 rm audit_*

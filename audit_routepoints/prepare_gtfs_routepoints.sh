9253c812d7c46947ca9bd800d96acefadefa56c1
Noémie <noemie.lehuby@zaclys.net>
2018-11-08 21:26:20 +0100

    STIF GTFS v3

--- audit_routepoints/prepare_gtfs_routepoints.sh
+++ audit_routepoints/prepare_gtfs_routepoints.sh
@@ -2,21 +2,21 @@
 
 echo "Extraction des routepoints GTFS"
 echo "##### on calcule des trip points #####"
-xsv join trip_id gtfs_trips.txt trip_id gtfs_audit_stoptime_lite.csv  |xsv select 'route_id,wheelchair_accessible,stop_id,direction_id,trip_id' > gtfs_audit_trippoints.csv
+xsv join trip_id gtfs_trips.txt trip_id gtfs_audit_stoptime_lite.csv  |xsv select 'route_id,wheelchair_accessible,stop_id,direction_id,trip_id,trip_headsign' > gtfs_audit_trippoints.csv
 
 echo "##### on ajoute les destinations et on filtre les duplicats #####"
-xsv join trip_id gtfs_audit_trippoints.csv trip_id gtfs_audit_trip_destinations.csv |xsv select 'route_id,wheelchair_accessible,stop_id,direction_id,trip_dest_id' |xsv sort |uniq > gtfs_audit_routepoints_1.csv
+cat gtfs_audit_trippoints.csv |xsv select 'route_id,wheelchair_accessible,stop_id,direction_id,trip_headsign' |xsv sort |uniq > gtfs_audit_routepoints_1.csv
+
+echo "##### on filtre les stop extensions #####"
+cat gtfs_stop_extensions.txt |xsv search -s object_system ZDEr_ID_REF_A |xsv select object_id,object_code > gtfs_audit_ref_stif.csv
 
 echo "##### on ajoute les infos de l'arrêt #####"
-xsv join stop_id gtfs_audit_routepoints_1.csv stop_id gtfs_stop_extensions.txt |xsv select '!stop_id[1]' > gtfs_audit_routepoints_2.csv
-xsv join stop_id gtfs_audit_routepoints_2.csv stop_id gtfs_stops.txt |xsv select 'stop_id,route_id,wheelchair_accessible,ZDEr_ID_REF_A,stop_name,stop_lat,stop_lon,wheelchair_boarding,trip_dest_id' > gtfs_audit_routepoints_3.csv
-
-echo "##### on ajoute le nom de l'arrêt de destination #####"
-xsv join trip_dest_id gtfs_audit_routepoints_3.csv stop_id gtfs_stops.txt |xsv select 'stop_id[0],route_id,wheelchair_accessible,ZDEr_ID_REF_A,stop_name[0],stop_lat[0],stop_lon[0],wheelchair_boarding[0],stop_name[1]' > gtfs_audit_routepoints_4.csv
+xsv join stop_id gtfs_audit_routepoints_1.csv object_id gtfs_audit_ref_stif.csv |xsv select '!object_id' > gtfs_audit_routepoints_2.csv
+xsv join stop_id gtfs_audit_routepoints_2.csv stop_id gtfs_stops.txt |xsv select 'stop_id,route_id,wheelchair_accessible,object_code,stop_name,stop_lat,stop_lon,wheelchair_boarding,trip_headsign' > gtfs_audit_routepoints_3.csv
 
 echo "##### on renomme pour éviter les doublons de colonnes #####"
 echo "stop_id,route_id,route_wheelchair,ZDEr_ID_REF_A,stop_name,stop_lat,stop_lon,stop_wheelchair,dest_name" > gtfs_audit_gtfs_routepoints.csv
-tail -n +2 gtfs_audit_routepoints_4.csv >> gtfs_audit_gtfs_routepoints.csv
+tail -n +2 gtfs_audit_routepoints_3.csv >> gtfs_audit_gtfs_routepoints.csv
 
 echo "##### on affiche les informations sur la ligne #####"
 xsv join route_id gtfs_audit_gtfs_routepoints.csv route_id gtfs_routes.txt | xsv select stop_id,route_id,route_wheelchair,ZDEr_ID_REF_A,stop_name,stop_lat,stop_lon,stop_wheelchair,dest_name,route_short_name,agency_id,route_type |xsv search -s route_type 3 |xsv select '!route_type' > gtfs_audit_routepoints_with_code.csv

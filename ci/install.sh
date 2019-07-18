f589bddcdd5d58da461dd47729e7923473b3f977
Noémie <noemie.lehuby@zaclys.net>
2019-07-18 10:58:11 +0200

    update osm-transit-extractor

--- ci/install.sh
+++ ci/install.sh
@@ -7,8 +7,8 @@ wget https://bintray.com/artifact/download/snowplow/snowplow-generic/factotum_0.
 unzip factotum_0.5.0_linux_x86_64.zip
 
 # install osm-transit-extractor
-wget https://github.com/CanalTP/osm-transit-extractor/releases/download/v0.1.0/osm_transit_extractor_v0.1.0-x86_64-linux.zip
-unzip osm_transit_extractor_v0.1.0-x86_64-linux.zip
+wget https://github.com/CanalTP/osm-transit-extractor/releases/download/v0.2.1/osm_transit_extractor_v0.2.1-x86_64-linux.zip
+unzip osm_transit_extractor_v0.2.1-x86_64-linux.zip
 
 # install xsv
 wget https://github.com/BurntSushi/xsv/releases/download/0.13.0/xsv-0.13.0-x86_64-unknown-linux-musl.tar.gz

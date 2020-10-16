f313125c38368134256f43e81f9dd62ee3b79f01
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-16 10:34:52 +0000

    Deploy Jungle-Bus/ref-fr-STIF to github.com/Jungle-Bus/ref-fr-STIF.git:gh-pages

--- missing_in_osm/prepare_missing.sh
+++ missing_in_osm/prepare_missing.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "##### on prépare les lignes opendata #####"
-xsv join agency_id gtfs_routes.txt agency_id gtfs_agency.txt |xsv select agency_id,agency_name,route_id,route_short_name,route_long_name,route_type,route_color,route_text_color > opendata_lignes.csv
+xsv join agency_id gtfs_routes.txt agency_id gtfs_agency.txt |xsv select agency_id,agency_name,route_id,route_short_name,route_long_name,route_type,route_color,route_text_color |xsv sort -s route_id|xsv sort -s agency_name > opendata_lignes.csv
 
 echo "##### on récupère l'id OSM des lignes opendata #####"
-xsv join --left route_id opendata_lignes.csv osm:ref:FR:STIF:ExternalCode_Line lignes.csv|xsv select 1-8,line_id |xsv sort -s line_id|xsv sort -s agency_name > opendata_lines_with_osm_id.csv
+xsv join --left route_id opendata_lignes.csv osm:ref:FR:STIF:ExternalCode_Line lignes.csv|xsv select 1-8,line_id > opendata_lines_with_osm_id.csv
 # /!\ on a parfois plusieurs lignes OSM pour une même ligne opendata
 
 echo "##### on liste les lignes OSM avec un tracé #####"

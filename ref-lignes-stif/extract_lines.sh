4b05e57d66363317a0388972f14e349a2895e135
Noémie <noemie.lehuby@zaclys.net>
2020-10-17 19:04:07 +0200

    filter more networks

--- ref-lignes-stif/extract_lines.sh
+++ ref-lignes-stif/extract_lines.sh
@@ -2,7 +2,7 @@
 
 cat osm-transit-extractor_lines.csv \
  |xsv search -s 'osm:service' 'international' -v \
- |xsv search -s network TGV -v | xsv search -s network Eurostar -v \
+ |xsv search -s network TGV -v |xsv search -s network InOui -v | xsv search -s network Eurostar -v \
  |xsv search -s network Thalys -v |xsv search -s network Flixbus -v \
  |xsv search -s network Intercités -v|xsv search -s operator 'SNCF Réseau' -v \
  |xsv search -s network 'TER ' -v |xsv search -s operator 'DB Fernverkehr' -v \
@@ -13,5 +13,5 @@ cat osm-transit-extractor_lines.csv \
  |xsv search -s network 'Les Abeilles' -v | xsv search -s network 'Navettes Aéroports De Paris' -v \
  |xsv search -s network 'TransCape' -v | xsv search -s network Balabus -v \
  |xsv search -s network 'Big Bus' -v| xsv search -s network 'Carré 92' -v \
- |xsv search -s network 'Vexin Bus' -v | xsv search -s operator "Plus de Pep's" -v \
+ |xsv search -s network 'Vexin Bus' -v | xsv search -s network "Plus de Pep's" -v \
  |xsv select line_id,code,name,network,operator,colour,osm:type,mode,osm:ref:FR:STIF:ExternalCode_Line

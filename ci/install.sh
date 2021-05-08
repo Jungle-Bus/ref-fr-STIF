e4df651de7b81df052319016c03296e0ec1c593b
Noémie Lehuby <noemie.lehuby@zaclys.net>
2021-05-08 13:58:05 +0200

    Migrate the pipeline
    So long, Travis, and thanks for all the fish

--- ci/install.sh
+++ ci/install.sh
@@ -3,11 +3,11 @@
 set -ev
 
 # install osm-transit-extractor
-wget https://github.com/CanalTP/osm-transit-extractor/releases/download/v0.3.0/osm_transit_extractor_v0.3.0-x86_64-linux.zip
+wget --quiet https://github.com/CanalTP/osm-transit-extractor/releases/download/v0.3.0/osm_transit_extractor_v0.3.0-x86_64-linux.zip
 unzip osm_transit_extractor_v0.3.0-x86_64-linux.zip
 
 # install xsv
-wget https://github.com/BurntSushi/xsv/releases/download/0.13.0/xsv-0.13.0-x86_64-unknown-linux-musl.tar.gz
+wget --quiet https://github.com/BurntSushi/xsv/releases/download/0.13.0/xsv-0.13.0-x86_64-unknown-linux-musl.tar.gz
 tar -zxvf xsv-0.13.0-x86_64-unknown-linux-musl.tar.gz
 
 # install python dependencies

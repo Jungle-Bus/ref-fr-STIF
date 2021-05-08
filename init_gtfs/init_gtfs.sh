6a0f18ae19965caec3b503e975317b69d48b7ea5
nlehuby <nlehuby@users.noreply.github.com>
2021-05-08 12:47:50 +0000

    deploy: e4df651de7b81df052319016c03296e0ec1c593b

--- init_gtfs/init_gtfs.sh
+++ init_gtfs/init_gtfs.sh
@@ -5,7 +5,7 @@ set -euvo pipefail
 # Initialisation des données GTFS
 
 ## téléchargement du GTFS
-wget https://eu.ftp.opendatasoft.com/stif/GTFS/IDFM_gtfs.zip -O fr-idf_oif_stif.zip
+wget --quiet https://eu.ftp.opendatasoft.com/stif/GTFS/IDFM_gtfs.zip -O fr-idf_oif_stif.zip
 
 ## dézip des fichiers
 unzip fr-idf_oif_stif.zip -d gtfs
